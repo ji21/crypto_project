216262dab243531956613de93fec2e8727e9c00c
Jeffrey Ip <jeffreyip54@gmail.com>
2020-08-26 02:18:21 +0800

    quick fixes to my very bizzare html

--- config/settings.py
+++ config/settings.py
@@ -130,9 +130,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_FINDERS = [
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# ]
+
+STATIC_ROOT = ''
+# STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# print(STATICFILES_DIR)
 # STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 
+STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = ( os.path.join('static'), )
+
+# print(STATICFILES_DIRS)
 
