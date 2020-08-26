5e0749771107fca557d6ecf28c7187f0f3ff9bfa
Jeffrey Ip <jeffreyip54@gmail.com>
2020-08-26 16:32:01 +0800

    fixed nav bar to be sticky on top

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+//https://www.w3schools.com/howto/howto_js_navbar_sticky.asp
+const adjustHeight = () => {
+  const fadeHeight = document.querySelector("#fade-height");
+  fadeHeight.classList.remove("hidden");
+}
+
+const tab = document.querySelector("#myTab")
+tab.addEventListener("click", (event)=> {
+  if (event.srcElement.getAttribute("data-toggle")==="tab") {
+    adjustHeight();
+  }
+})
