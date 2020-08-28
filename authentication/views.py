b118ac39c449a364f5a46a935164ff5a05e62370
Jeffrey Ip <jeffreyip54@gmail.com>
2020-08-28 22:38:36 +0800

    register form validations completed

--- authentication/views.py
+++ authentication/views.py
@@ -4,6 +4,7 @@ from django.views import View
 from django.contrib.auth.models import User
 from django.contrib import messages
 from validate_email import validate_email
+from .models import Profile
 import json
 # Create your views here.
 class LoginView(View):
@@ -25,12 +26,15 @@ class RegView(View):
 
     if not User.objects.filter(username=username).exists():
       if not User.objects.filter(email=email).exists():
-        user = User.objects.create_user(username=username, email=email, full_name=full_name, phone=phone)
+        user = User.objects.create_user(username=username, email=email)
         user.set_password(password)
         user.save()
         messages.success(request, 'Your account has been successfully created.')
         return render(request, 'authenticate/register.html')
-    messages.error(request, 'Please fill in all required fields.')
+      else:
+        messages.error(request, 'Email already in use. Please use another email.')
+    if User.objects.filter(username=username).exists():
+      messages.error(request, 'Username has already been taken. Please choose another one.')
     return render(request, 'authenticate/register.html')
 
 
@@ -40,11 +44,11 @@ class UserValidView(View):
     data = json.loads(request.body)
     username = data['username']
 
-    if str(username).isalnum():
-      return JsonResponse({'username_valid': True})
+    if not str(username).isalnum():
+      return JsonResponse({'username_error': 'Username should not contain any non-alphanumerical characters.'}, status=400)
     elif User.objects.filter(username=username).exists():
       return JsonResponse({'username_error': 'Username has already been taken.'}, status=400)
-    return JsonResponse({'username_error': 'Username should not contain any non-alphanumerical characters.'}, status=400)
+    return JsonResponse({'username_valid': True})
 
 
 class EmailValidView(View):
@@ -52,8 +56,8 @@ class EmailValidView(View):
     data=json.loads(request.body)
     email = data['email']
 
-    if validate_email(email):
-      return JsonResponse({'email_valid': True})
-    elif User.objects.filter(email=email).exists():
+    if User.objects.filter(email=email).exists():
       return JsonResponse({'email_error': 'Email has already been taken.'}, status=400)
+    elif validate_email(email):
+      return JsonResponse({'email_valid': True})
     return JsonResponse({'email_error': 'Email format is invalid.'}, status=400)
