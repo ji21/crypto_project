13a06f0ad23bb157afcf4546ea08c25a3355cae4
Jeffrey Ip <jeffreyip54@gmail.com>
2020-09-10 21:36:34 +0800

    made migration - added account model

--- authentication/models.py
+++ authentication/models.py
@@ -8,8 +8,12 @@ class Profile(models.Model):
   phone = models.CharField(max_length=20, blank=True, null=True)
   user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
+
+class Account(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='accounts', null=True)
+
 class Transaction(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transaction', null=True)
+  account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='transactions', null=True)
   amount_bought = models.IntegerField(blank=False)
   transaction_fee = models.IntegerField(blank=True, null=True, editable=True)
   created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -20,5 +24,9 @@ class Transaction(models.Model):
   #market price is the exchange rate at the time of creation
 
 class Wallet(models.Model):
-  owner = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+  account = models.OneToOneField(Account, on_delete=models.CASCADE, null=True)
   balance = models.IntegerField()
+
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'channels',
+    'preferences'
 ]
 
 MIDDLEWARE = [
